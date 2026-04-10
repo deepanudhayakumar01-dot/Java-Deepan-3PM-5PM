300e3832a1a2b10afca6c2610a992467f2ffd93d
DEEPAN U <deepanudhayakumar01@gmail.com>
2026-04-10 17:35:35 +0530

    update

---
+++ JDBC Assignment-2/Task-4.java
@@ -0,0 +1,27 @@
+package jdbcAssignment2;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Assignment4 {
+	static final String DB_URL="jdbc:mysql://localhost:3306/JDBCAssignment_1";
+	static final String USER= "root";
+	static final String PASS = "admin";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try(Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);) {
+			CallableStatement cs=conn.prepareCall("call updatestundents(?,?)");
+			cs.setInt(1, 3);
+			cs.setInt(2, 90);
+			cs.execute();
+			System.out.println("Your record has done");
+			
+		}catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
